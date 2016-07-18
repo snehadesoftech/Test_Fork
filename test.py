fa89ff3cd4cf79c0ad748aa77856df3db4918ccf
SnehaDL <sneha.dabbadi@gmail.com>
2016-07-18 16:50:10 +0530

    making changes

--- test.py
+++ test.py
@@ -16,3 +16,11 @@ if list(my_str) == list(rev_str):
    print("It is palindrome")
 else:
    print("It is not palindrome")
+
+def bubble_sort(items):
+        """ Implementation of bubble sort """
+        for i in range(len(items)):
+                for j in range(len(items)-1-i):
+                        if items[j] &gt; items[j+1]:
+                                items[j], items[j+1] = items[j+1], items[j]     # Swap!
+ 
